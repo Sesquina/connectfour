d771a9711b7f4b52dc6a8f7274366c275f22cc19
Samira Esquina <samiraesquina@gmail.com>
2020-07-06 11:06:46 -0400

    Created JS Grid

--- connect4.js
+++ connect4.js
@@ -9,3 +9,23 @@ class Connect4 {
       this.createGrid();
       this.setupEventListeners();
     }
+
+    createGrid() {
+        const $board = $(this.selector);
+        $board.empty();
+        this.isGameOver = false;
+        this.player = 'red';
+        for (let row = 0; row < this.ROWS; row++) {
+          const $row = $('<div>')
+            .addClass('row');
+          for (let col = 0; col < this.COLS; col++) {
+            const $col = $('<div>')
+              .addClass('col empty')
+              .attr('data-col', col)
+              .attr('data-row', row);
+            $row.append($col);
+          }
+          $board.append($row);
+        }
+      }
+    
